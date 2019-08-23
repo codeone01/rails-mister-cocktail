541fc8bb85f59fa73bd65b9aafc1750940e68612
Jose Eduardo Azevedo <eduardoazevedo08@gmail.com>
2019-08-23 14:54:13 -0300

    first site

---
+++ app/models/cocktail.rb
@@ -0,0 +1,8 @@
+class Cocktail < ApplicationRecord
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
+
+  validates_uniqueness_of :name
+
+  validates :name, presence: true
+end
---
+++ app/models/ingredient.rb
@@ -0,0 +1,7 @@
+class Ingredient < ApplicationRecord
+  has_many :doses
+
+  validates_uniqueness_of :name
+
+  validates :name, presence: true
+end
